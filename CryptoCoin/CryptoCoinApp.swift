a593c09dffc25081563fdc2c3630281c48391bcb
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-10-19 22:53:01 +0300

    Initial Commit

---
+++ CryptoCoin/CryptoCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCoinApp.swift
+//  CryptoCoin
+//
+//  Created by Amby on 19/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
